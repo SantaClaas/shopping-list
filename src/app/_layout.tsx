71d58ed54a8847108be50d15b61ddba48e7f56ca
Claas <claas@claas.dev>
2024-03-28 22:38:55 +0100

    Remove entry point from before navigation was added

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,30 +1,38 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import TopAppBar from "../components/navigation/TopAppBar";
 import theme from "../theme";
+
+const queryClient = new QueryClient();
 
 export default function () {
   console.log("theme", theme.colors.light.surface);
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: theme.colors.light.surface,
-        },
-        headerShadowVisible: false,
-        headerTintColor: theme.colors.light.on.surface,
-        contentStyle: {
-          backgroundColor: theme.colors.light.surface,
-        },
-        header: TopAppBar,
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{
-          headerTitle: "Shopping Lists",
-        }}
-      />
-      <Stack.Screen name="lists/[id]" options={{ headerTitle: "List" }} />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <QueryClientProvider client={queryClient}>
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: theme.colors.light.surface,
+            },
+            headerShadowVisible: false,
+            headerTintColor: theme.colors.light.on.surface,
+            contentStyle: {
+              backgroundColor: theme.colors.light.surface,
+            },
+            header: TopAppBar,
+          }}
+        >
+          <Stack.Screen
+            name="index"
+            options={{
+              headerTitle: "Shopping Lists",
+            }}
+          />
+          <Stack.Screen name="lists/[id]" options={{ headerTitle: "List" }} />
+        </Stack>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   );
 }
